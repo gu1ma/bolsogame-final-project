c4d8b858243bd16b5cb4c0802e7dcc5004d7d49b
Gabriel Guimarães <gabrielgs1408@gmail.com>
2020-07-09 20:26:55 -0300

    feat: change orientation of player on left or right

--- Assets/Scripts/Bolsonaro.cs
+++ Assets/Scripts/Bolsonaro.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class Bolsonaro : MonoBehaviour
 {
     public float speed;
+    private float input;
 
     Rigidbody2D rb;
     // Start is called before the first frame update
@@ -13,11 +14,19 @@ public class Bolsonaro : MonoBehaviour
         rb = GetComponent<Rigidbody2D>();
     }
 
+    private void Update() {
+        if(input > 0) {
+            transform.eulerAngles = new Vector3(0, 0, 0);
+        } else {
+            transform.eulerAngles = new Vector3(0, 180, 0);
+        }
+    }
+
     // Update is called once per frame
     void FixedUpdate()
     {
         //store player input
-        float input = Input.GetAxisRaw("Horizontal");
+        input = Input.GetAxisRaw("Horizontal");
         
         //Move the player
         rb.velocity = new Vector2(input * speed, rb.velocity.y);
