b65a639dcce00bbcf94f1dc06bcee2a19720736c
Gabriel Guimarães <gabrielgs1408@gmail.com>
2020-07-09 20:05:20 -0300

    feat: move player and prevent overflow screen

---
+++ Assets/Scripts/Bolsonaro.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bolsonaro : MonoBehaviour
+{
+    public float speed;
+
+    Rigidbody2D rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        //store player input
+        float input = Input.GetAxisRaw("Horizontal");
+        
+        //Move the player
+        rb.velocity = new Vector2(input * speed, rb.velocity.y);
+    }
+}
